7f443ab223d3dbaaa9b3988a75fb2a9d0b04059c
Aleks <yetanothermodder@pm.me>
2025-11-22 02:52:08 +0100

    Allow varying definition versions.

--- kaos/data/__init__.py
+++ kaos/data/__init__.py
@@ -9,6 +9,9 @@ _FOLDER   = Path(__file__).parent
 _DEF_FILE = "definitions.json"
 
 def get_definitions() -> dict[str, dict]:
+    if not (_FOLDER / _DEF_FILE).is_file():
+        return {}
+    
     with open(_FOLDER / _DEF_FILE, 'r') as file:
         return json.load(file)
     
@@ -17,10 +20,13 @@ def add_definitions(definitions: list[Definition]) -> None:
     for defn in definitions:
         if defn is None:
             continue
+        
         if defn.name not in existing:
-            existing[defn.name] = defn.to_dict()
-        if defn.version != existing[defn.name]["version"]:
-            print(f"{defn.name} Version Difference: {existing[defn.name]['version']} (Stored), {defn.version} (Read)")
-            
+            existing[defn.name] = {defn.version: defn.to_dict()}
+        
+        elif str(defn.version) not in existing[defn.name]:
+            existing[defn.name][str(defn.version)] = defn.to_dict()
+
     with open(_FOLDER / _DEF_FILE, 'w') as file:
         file.write(json.dumps(existing, indent=4))
+
